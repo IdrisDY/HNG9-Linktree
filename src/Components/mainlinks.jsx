3636d71e97aa874a627e6099ebad965eb7581265
DisuAde <idrisadekunle726@gmail.com>
2022-11-03 19:21:55 +0100

    test;anchor link check

--- src/Components/mainlinks.jsx
+++ src/Components/mainlinks.jsx
@@ -33,8 +33,8 @@ useEffect(()=>{
    <a id='' href='https://books.zuri.team/python-for-beginners?ref_id=<DisuAde>' className='links'> Python Books</a>
    <a href='https://background.zuri.team' id='pitch' className='links'> Background Check for Coders</a>
    <a href='https://books.zuri.team/design-rules ' id='book__design' className='links'  > Design Books</a>
-  {/* <a href='/contact' id='contact' className='links'>Contact</a> */}
-   <Link to='/contact' className='links'>Contact</Link>
+  <a href='/contact' id='contact' className='links'>Contact</a>
+   {/* <Link to='/contact' className='links'>Contact</Link> */}
 </div>
 
 <div className='logos'> 
